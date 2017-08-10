943e8da7fcf507208d66ff01fb47581b30f2f983
Dan McCune <dmccune@hivelocity.net>
2017-08-10 14:41:54 -0400

    form submit check/post

--- php-form.php
+++ php-form.php
@@ -4,10 +4,26 @@
     <title>PHP Form</title>
 </head>
 <body>
+<?php
+    if (isset($_POST['submit'])) {
+        // process form
+        printf('User name: %s
+        <br />Password: %s
+        <br />Gender: %s
+        <br />Color: %s
+        <br />Language(s): %s
+        <br />Comments: %s
+        <br />T&amp;C: %s',
+            $_POST['name'],
+            $_POST['password'],'','','',
+            $_POST['comments'],'');
+    }
+//echo $_POST['pwd'];
+?>
 <form method="post" action=''>
 
-    Username:<input type='text' name='txt' /><br />
-    Password:<input type='password' name='pwd' /><br />
+    Username:<input type='text' name='name' /><br />
+    Password:<input type='password' name='password' /><br />
     Gender:
         <input type='radio' name ='gender' value="f" />Female
         <input type='radio' name ='gender' value="m" />Male<br />
@@ -24,7 +40,7 @@
             <option value="it">Italian</option>
         </select><br />
     Comments: <textarea name="comments"></textarea><br />
-    <input type="checkbox" name="tc" value="ok" />I accept the T&amp;C< br />
+    <input type="checkbox" name="tc" value="ok" />I accept the T&amp;C<br />
     <input type="submit" name="submit" value="Submit">
 </form>
 </body>
