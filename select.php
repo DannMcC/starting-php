74d797b1995d8d7eca11fc543f1e91c90da2745d
Dan McCune <dmccune@hivelocity.net>
2017-08-17 14:39:04 -0400

    query database and return HTML

---
+++ select.php
@@ -0,0 +1,27 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title>PHP</title>
+</head>
+
+<body>
+
+<ul>
+
+    <?php
+    $db = mysqli_connect('localhost', 'root', '', 'php');
+    $sql = 'SELECT * FROM users';
+    $result = mysqli_query($db, $sql);
+
+    foreach ($result as $row) {
+        printf('<li><span style="color: %s;"> %s (%s)</span></li>',
+            htmlspecialchars($row['color']),
+            htmlspecialchars($row['name']),
+            htmlspecialchars($row['gender'])
+        );
+    }
+    ?>
+
+</ul>
+</body>
+</html>
